fd5387a755c90929018c8c0c5bd8097c95fd7cee
rohit-shah840 <rohitshah1455@gmail.com>
2024-01-29 16:53:27 +0530

    Update employee controller to improve error handling

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -9,7 +9,7 @@ router.get("/", async (req, res) => {
   res.send(employees);
 });
 
-//url for the below ===> http://localhost:3000/api/employees/
+//url for the below ===> http://localhost:3000/api/employees/{id}
 router.get("/:id", async (req, res) => {
   const employee = await service.getEmployeeById(req.params.id);
   if (employee.length == 0)
@@ -17,7 +17,7 @@ router.get("/:id", async (req, res) => {
   else res.send(employee);
 });
 
-//url for the below ===> http://localhost:3000/api/employees/
+//url for the below ===> http://localhost:3000/api/employees/{id}
 router.delete("/:id", async (req, res) => {
   const affectedRows = await service.deleteEmployeeById(req.params.id);
   if (affectedRows == 0)
@@ -31,7 +31,7 @@ router.post("/", async (req, res) => {
   res.status(201).send("data inserted successfully!!");
 });
 
-//url for the below ===> http://localhost:3000/api/employees/
+//url for the below ===> http://localhost:3000/api/employees/{id}
 router.put("/:id", async (req, res) => {
   const affectedRows = await service.addOrEditEmployee(req.body, req.params.id);
   if (affectedRows == 0)
@@ -40,3 +40,5 @@ router.put("/:id", async (req, res) => {
 });
 
 module.exports = router;
+
+//note in the {id} you should palced integer value as id of employee.
